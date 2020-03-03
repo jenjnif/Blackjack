c3024e4157d760e57f26dea8fa303233e7ff1329
jenjnif <jenjnif@hotmail.com>
2020-03-03 11:52:21 +0000

    Add chips class

--- black_jack_udemy.py
+++ black_jack_udemy.py
@@ -114,6 +114,19 @@ class Hand():
             self.aces -= 1
 
 
+class Chips():
+
+    def __init__(self, total=100):
+        self.total = total
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
+
+
 new_deck = Deck()
 new_deck.shuffle()
 print(new_deck)
