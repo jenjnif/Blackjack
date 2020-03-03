65915a5792e9a7570173c4300175b095ddb72faa
jenjnif <jenjnif@hotmail.com>
2020-03-03 12:01:53 +0000

    Add take_bet function

--- black_jack_udemy.py
+++ black_jack_udemy.py
@@ -127,6 +127,20 @@ class Chips():
         self.total -= self.bet
 
 
+def take_bet(chips):
+
+    while True:
+        try:
+            chips.bet = int(input("How many chips would you like to bet?"))
+        except:
+            print("Sorry, please provide an integer.")
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, you do not have enough chips! You have: {}".format(chips.total))
+            else:
+                break
+
+
 new_deck = Deck()
 new_deck.shuffle()
 print(new_deck)
